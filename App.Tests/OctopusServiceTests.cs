daa576611e9634b62ce00a124e8adb0f488ff3e9
skc <skc@scopito.com>
2026-04-09 09:14:48 +0200

    feat: add remaining test for octopusService

--- App.Tests/OctopusServiceTests.cs
+++ App.Tests/OctopusServiceTests.cs
@@ -1,3 +1,4 @@
+using BlazorApp.Models;
 using BlazorApp.Services;
 
 namespace App.Tests;
@@ -6,13 +7,116 @@ public class OctopusServiceTests
 {
     private readonly OctopusService _service = new();
 
-    [Fact]
-    public void OctopusCsvToEntities_WithValidInput_Works()
+
+    /**
+     * Helper method for mocking the frontend passing a file to the
+     * OctopusCsvToEntities method
+     */
+    private FileStream GetFileFromPath(string filePath)
     {
-        // Næste gang skal vi sige på forhånd, hvordan vores objekter af klassen 
-        // "vare" skal se ud, og derefter indlæse og lave de objekter gennem 
-        // metoden og matche dem op med hinanden.
-        var filePath = "TestData/OctopusTestData.csv"; 
-        var result = _service.OctopusCsvToEntities(filePath);
+        return File.OpenRead(filePath);
+    }
+
+    [Fact]
+    public void OctopusCsvToEntities_WithValidInput_ReturnsExpectedResult()
+    {
+        var expected = new List<Vare>
+        {
+            new Vare
+            {
+                Varenr = "13900",
+                Varetekst = "4x50 cl Amarcord i gavekasse",
+                Tekst2 = "",
+                Lokation = "",
+                Leverandor = "",
+                Beholdning = -24,
+                Prisfaktor = 1,
+                Gennemsnit = 70,
+                Lagervaerdi = -1680.00m,
+                Genanskaffelse = 70,
+                LagervaerdiGenanskaffelse = -1680.00m,
+                Varegruppe = "80",
+                Undergruppe = "10",
+                Lagergruppe = "10",
+                SidsteSalgsdato = new DateTime(2026, 1, 14),
+                SidsteKobsdato = new DateTime(2021, 12, 7),
+                Edbnr = "00000",
+                Enhed = "STK",
+                Type = "P",
+                Disponibel = -24
+            },
+            new Vare
+            {
+                Varenr = "14500",
+                Varetekst = "Pallegavekassen i træ",
+                Tekst2 = "med 10 x 50 cl Klosterbryg",
+                Lokation = "",
+                Leverandor = "",
+                Beholdning = -1,
+                Prisfaktor = 1,
+                Gennemsnit = 180,
+                Lagervaerdi = -180,
+                Genanskaffelse = 180,
+                LagervaerdiGenanskaffelse = -180,
+                Varegruppe = "80",
+                Undergruppe = "10",
+                Lagergruppe = "10",
+                SidsteSalgsdato = new DateTime(2026, 1, 23),
+                SidsteKobsdato = new DateTime(2021, 12, 7),
+                Edbnr = "14500",
+                Enhed = "STK",
+                Type = "P",
+                Disponibel = -1
+            },
+            new Vare
+            {
+                Varenr = "14520",
+                Varetekst = "Trækasse med 6 x 50 cl",
+                Tekst2 = "Klosterbryg",
+                Lokation = "",
+                Leverandor = "",
+                Beholdning = -7,
+                Prisfaktor = 1,
+                Gennemsnit = 0,
+                Lagervaerdi = 0,
+                Genanskaffelse = 140,
+                LagervaerdiGenanskaffelse = -980,
+                Varegruppe = "80",
+                Undergruppe = "10",
+                Lagergruppe = "10",
+                SidsteSalgsdato = new DateTime(2026, 2, 11),
+                SidsteKobsdato = new DateTime(2021, 12, 7),
+                Edbnr = "14520",
+                Enhed = "STK",
+                Type = "P",
+                Disponibel = -7
+            }
+        };
+
+        var filePath = "TestData/OctopusTestData.csv";
+        var stream = GetFileFromPath(filePath);
+        var result = _service.OctopusCsvToEntities(stream);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void OctopusCsvToEntities_WithHeaderOnly_ReturnsEmptyList()
+    {
+        var stream = GetFileFromPath("TestData/OctopusTestDataHeaderOnly.csv");
+        var result = _service.OctopusCsvToEntities(stream);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void OctopusCsvToEntities_WithMissingDates_ReturnsNullDates()
+    {
+        var stream = GetFileFromPath("TestData/OctopusTestDataMissingDates.csv");
+        var result = _service.OctopusCsvToEntities(stream);
+
+        Assert.Single(result);
+        Assert.Null(result[0].SidsteSalgsdato);
+        Assert.Null(result[0].SidsteKobsdato);
     }
 }
--- App/Models/Vare.cs
+++ App/Models/Vare.cs
@@ -22,4 +22,43 @@ public class Vare
     public string Enhed { get; set; }
     public string Type { get; set; }
     public decimal Disponibel { get; set; }
+
+    protected bool Equals(Vare other)
+    {
+        return Varenr == other.Varenr && Varetekst == other.Varetekst && Tekst2 == other.Tekst2 && Lokation == other.Lokation && Leverandor == other.Leverandor && Beholdning == other.Beholdning && Prisfaktor == other.Prisfaktor && Gennemsnit == other.Gennemsnit && Lagervaerdi == other.Lagervaerdi && Genanskaffelse == other.Genanskaffelse && LagervaerdiGenanskaffelse == other.LagervaerdiGenanskaffelse && Varegruppe == other.Varegruppe && Undergruppe == other.Undergruppe && Lagergruppe == other.Lagergruppe && Nullable.Equals(SidsteSalgsdato, other.SidsteSalgsdato) && Nullable.Equals(SidsteKobsdato, other.SidsteKobsdato) && Edbnr == other.Edbnr && Enhed == other.Enhed && Type == other.Type && Disponibel == other.Disponibel;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Vare)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(Varenr);
+        hashCode.Add(Varetekst);
+        hashCode.Add(Tekst2);
+        hashCode.Add(Lokation);
+        hashCode.Add(Leverandor);
+        hashCode.Add(Beholdning);
+        hashCode.Add(Prisfaktor);
+        hashCode.Add(Gennemsnit);
+        hashCode.Add(Lagervaerdi);
+        hashCode.Add(Genanskaffelse);
+        hashCode.Add(LagervaerdiGenanskaffelse);
+        hashCode.Add(Varegruppe);
+        hashCode.Add(Undergruppe);
+        hashCode.Add(Lagergruppe);
+        hashCode.Add(SidsteSalgsdato);
+        hashCode.Add(SidsteKobsdato);
+        hashCode.Add(Edbnr);
+        hashCode.Add(Enhed);
+        hashCode.Add(Type);
+        hashCode.Add(Disponibel);
+        return hashCode.ToHashCode();
+    }
 }
