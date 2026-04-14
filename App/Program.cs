ec8479c4635ef2d9633ce98a3488821090b9a746
Magnusorskov <magnusorskov@hotmail.com>
2026-04-14 09:37:01 +0200

    feat: change gui to match an upload service instead of convert service

--- App/Program.cs
+++ App/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<ICsvConverterService, CsvConverterService>();
+builder.Services.AddScoped<ICsvUploadService, CsvUploadService>();
 
 var app = builder.Build();
 
